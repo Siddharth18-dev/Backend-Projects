31dce9cf733939b48df9b27b73d1232b3fc0e8cd
Siddharth18-dev <siddharthjha069@gmail.com>
2025-09-02 05:23:43 +0000

    Tried to create Server and Learn't routings, express.route

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,16 @@
 import express from "express";
+import routing from "./routes/Routing.js";
 
 const app = express();
-const PORT = 5001;
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, (req, res) => {
+    console.log(`Server is running on PORT: http://localhost:${PORT}`);
+});
 
 app.get("/", (req, res) => {
-    res.send("Hello Siddharth JHA");
+    res.send("Hey! Hi i'm alive");
 });
 
-app.get("/About", (req, res) => {
-    res.send("Hello ")
-})
-// Start the server on port 5001
-app.listen(PORT, () => {   
-    console.log(`Server has listening on : http://localhost:${PORT}`);
-});
+app.use("/user", routing);
+
