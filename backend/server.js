2fb6ce82c5a5ad54058e8ec3af0825ce703ddf9f
Siddharth18-dev <siddharthjha069@gmail.com>
2025-09-28 14:06:30 +0000

    Learnt about MongoDB CRUD Operations and Routing principles, mongoose Schema and Others

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import routing from "./routes/Routing.js";
-
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5001;
+connectDB();
 
 app.listen(PORT, (req, res) => {
     console.log(`Server is running on PORT: http://localhost:${PORT}`);
@@ -11,6 +14,9 @@ app.listen(PORT, (req, res) => {
 app.get("/", (req, res) => {
     res.send("Hey! Hi i'm alive");
 });
+
+//Middleware
+app.use(express.json());
 
 app.use("/user", routing);
 
